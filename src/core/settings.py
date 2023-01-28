2e14b20aa32c6b0a0193de319c4819cbab5e0a4a
Hosam Almoghraby <hosam.moghraby@gmail.com>
2023-01-28 03:45:01 +0000

    from secrets to vars

--- src/core/settings.py
+++ src/core/settings.py
@@ -125,7 +125,7 @@ USE_TZ = os.environ.get("DJ_USE_TZ") == "True"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = "static/"
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
